e230271a95f90a6cb22b4d2e0027ec75bbd0f5af
linaresg <82194867+linaresg@users.noreply.github.com>
2021-11-30 13:18:56 -0600

    Add files via upload

---
+++ Code.php
@@ -0,0 +1,46 @@
+<?php
+if($_REQUEST['cod']==1)
+	echo "<p><strong>Descripcion: </strong> Detergente Bolivar</p>
+	<p><strong>Precio: </strong> S/. 60.00</p>
+	<p><strong>Detalle: </strong> 20kg.</p>";
+if($_REQUEST['cod']==2)
+	echo "<p><strong>Descripcion: </strong> Lejìa Clorox</p>
+	<p><strong>Precio: </strong> S/. 9.00</p>
+	<p><strong>Detalle: </strong> 5l.</p>";
+if($_REQUEST['cod']==3)
+	echo "<p><strong>Descripcion: </strong> Jabòn Bolivar</p>
+	<p><strong>Precio: </strong> S/. 2.00</p>
+	<p><strong>Detalle: </strong> 500g.</p>";
+if($_REQUEST['cod']==4)
+	echo "<p><strong>Descripcion: </strong> Shampoo Sedal</p>
+	<p><strong>Precio: </strong> S/. 14.00</p>
+	<p><strong>Detalle: </strong> 1l.</p>";
+if($_REQUEST['cod']==5)
+	echo "<p><strong>Descripcion: </strong> Jabòn Tocador Nivea</p>
+	<p><strong>Precio: </strong> S/. 5.00</p>
+	<p><strong>Detalle: </strong> 350g.</p>";
+if($_REQUEST['cod']==6)
+	echo "<p><strong>Descripcion: </strong> Acondicionador Sedal</p>
+	<p><strong>Precio: </strong> S/. 10.00</p>
+	<p><strong>Detalle: </strong> 1l.</p>";
+if($_REQUEST['cod']==7)
+	echo "<p><strong>Descripcion: </strong> Pasta Dental Colgate</p>
+	<p><strong>Precio: </strong> S/. 9.00</p>
+	<p><strong>Detalle: </strong> 400g.</p>";
+if($_REQUEST['cod']==8)
+	echo "<p><strong>Descripcion: </strong> Hilo Dental Dento</p>
+	<p><strong>Precio: </strong> S/. 3.50</p>
+	<p><strong>Detalle: </strong> 120g.</p>";
+if($_REQUEST['cod']==9)
+	echo "<p><strong>Descripcion: </strong> Peine</p>
+	<p><strong>Precio: </strong> S/. 1.00</p>
+	<p><strong>Detalle: </strong> 50g.</p>";
+if($_REQUEST['cod']==10)
+	echo "<p><strong>Descripcion: </strong> Màquina de Afeitar Stick</p>
+	<p><strong>Precio: </strong> S/. 4.00</p>
+	<p><strong>Detalle: </strong> 100g.</p>";
+if($_REQUEST['cod']==11)
+	echo "<p><strong>Descripcion: </strong> Perfume CyZone</p>
+	<p><strong>Precio: </strong> S/. 45.00</p>
+	<p><strong>Detalle: </strong> 500ml.</p>";
+?>
